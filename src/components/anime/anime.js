bc8fb7cc18a83ce50f2cd5aa01bb06ca9b589f09
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2022-07-06 13:08:17 +0530

    Added Animation Wheel

--- src/components/anime/anime.js
+++ src/components/anime/anime.js
@@ -5,32 +5,7 @@ import "./anime.css";
 export default function anime() {
   return (
     <div className="animation_container">
-      <img src="./assets/C0.png" alt="logo" className="animation_circle" />
-      <img
-        src="./assets/C1.png"
-        alt="logo"
-        className="animation_icon animation_c1"
-      />
-      <img
-        src="./assets/C2.png"
-        alt="logo"
-        className="animation_icon animation_c2"
-      />
-      <img
-        src="./assets/C3.png"
-        alt="logo"
-        className="animation_icon animation_c3"
-      />
-      <img
-        src="./assets/C4.png"
-        alt="logo"
-        className="animation_icon animation_c4"
-      />
-      <img
-        src="./assets/C5.png"
-        alt="logo"
-        className="animation_icon animation_c5"
-      />
+      <img src="./assets/Cmain.png" alt="logo" className="animation_circle" />
     </div>
   );
 }
