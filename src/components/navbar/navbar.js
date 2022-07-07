855d9ca37ddd6009b7bf518a5e0f6bd1698a2ec6
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2022-07-07 10:42:52 +0530

    Added Modals and Finalize Design - 1

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,10 +2,42 @@ import React from "react";
 import { RiUser3Fill } from "react-icons/ri";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import Popover from "react-awesome-popover";
+import Modal from "react-modal";
 
 import "./navbar.css";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "transparent",
+    border: "none",
+    overflow: "hidden",
+    width: "70%",
+    backgroundColor: "#242526",
+    borderRadius: "20px",
+  },
+  overlay: {
+    background: "rgba(85,85,85,0.8)",
+  },
+};
+
 export default function Navbar() {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [modalStyle, setModalStyle] = React.useState(0);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   const Register = () => {
     return (
       <div className="form_container">
@@ -29,9 +61,6 @@ export default function Navbar() {
                 size={20}
               />
             </button>
-          </div>
-          <div>
-            <p className="form_p form_p_new">already a user</p>
           </div>
         </form>
       </div>
@@ -57,16 +86,91 @@ export default function Navbar() {
               />
             </button>
           </div>
-          <div>
-            <p className="form_p form_p_new">new user</p>
-          </div>
         </form>
       </div>
     );
   };
 
+  const MainModal = () => {
+    return (
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        {modalStyle ? (
+          <>
+            <div className="form_container_modal">
+              <form className="form_main">
+                <div>
+                  <p className="form_p">Email</p>
+                  <input className="form_text form_text_modal" type="email" />
+                </div>
+                <div>
+                  <p className="form_p">Password</p>
+                  <input
+                    className="form_text form_text_modal"
+                    type="password"
+                  />
+                </div>
+                <div>
+                  <p className="form_p">Confirm</p>
+                  <input
+                    className="form_text form_text_modal"
+                    type="password"
+                  />
+                </div>
+                <div>
+                  <button type="submit" className="submit_btn">
+                    <BsFillArrowRightCircleFill
+                      className="submit_btn_icon"
+                      size={20}
+                    />
+                  </button>
+                </div>
+                <div onClick={() => setModalStyle(0)}>
+                  <p className="form_p form_p_new">already a user</p>
+                </div>
+              </form>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="form_container_modal">
+              <form className="form_main">
+                <div>
+                  <p className="form_p">Email</p>
+                  <input className="form_text form_text_modal" type="email" />
+                </div>
+                <div>
+                  <p className="form_p">Password</p>
+                  <input
+                    className="form_text form_text_modal"
+                    type="password"
+                  />
+                </div>
+                <div>
+                  <button type="submit" className="submit_btn">
+                    <BsFillArrowRightCircleFill
+                      className="submit_btn_icon"
+                      size={20}
+                    />
+                  </button>
+                </div>
+                <div onClick={() => setModalStyle(1)}>
+                  <p className="form_p form_p_new">new user</p>
+                </div>
+              </form>
+            </div>
+          </>
+        )}
+      </Modal>
+    );
+  };
+
   return (
     <div>
+      <MainModal />
       <div className="sub_navbar">
         <div>
           <img src="./assets/logo.png" alt="logo" className="logo" />
@@ -126,7 +230,7 @@ export default function Navbar() {
         </div>
         <div className="navbar_options2">
           <div className="navbar_options2_user">
-            <RiUser3Fill size={19} />
+            <RiUser3Fill onClick={openModal} size={19} />
           </div>
           <div>
             <input type="checkbox" id="active" />
